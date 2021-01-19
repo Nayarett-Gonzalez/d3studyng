bb940cddd776ed2df3955e9f30b5df927ff959f4
NayarettGonzalez <nagonzalezv@uc.cl>
2021-01-19 16:20:47 -0300

    Se coloca un color de fondo

--- selection-2/app.js
+++ selection-2/app.js
@@ -1,4 +1,7 @@
-d3.selectAll("circle").style("fill", "green");
+d3.selectAll("svg").style("background", "lightpink");
+d3.selectAll("circle").style("fill", "none");
+d3.select("circle").style("fill", "green");
+d3.selectAll("circle").style("stroke", "#444444");
 d3.select("#two > circle").style("fill", "deeppink");
 d3.select("#two > circle:nth-child(2)").style("fill", "purple");
 d3.select("#three > circle:nth-child(3)").style("fill", "purple");
