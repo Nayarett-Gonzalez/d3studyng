37c5cec1458d126212539ab03adb45f30ef761bc
NayarettGonzalez <nagonzalezv@uc.cl>
2021-01-19 15:13:57 -0300

    d3.select(#id)

--- selection/app.js
+++ selection/app.js
@@ -1,10 +1,10 @@
 // d3.select: Seleccionando sólo el primer elemento del tipo señalado 
 d3.select("div").style("background", "#456789");
-d3.select("p").style("color", "green"); //Select only the first paragraph
+d3.select("p").style("color", "purple"); //Select only the first paragraph
 
 // d3.selectAll: Se seleccionan todos las divisiones
 d3.selectAll("div").style("color", "white");
 
 // Haciendo selección por #id
-d3.select("div#container1").style("background", "green");
-d3.select("span#inside").style("background", "green");
+d3.select("#container1").style("background", "green");
+d3.select("#inside").style("background", "green");
