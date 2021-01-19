2a6d8b5eb49394cca83fa5c86b85eebc809ef0a4
NayarettGonzalez <nagonzalezv@uc.cl>
2021-01-19 15:05:02 -0300

    d3.select() y d3.selectAll()

---
+++ selection/app.js
@@ -0,0 +1,8 @@
+// d3.select: Seleccionando sólo el primer elemento del tipo señalado 
+d3.select("div").style("background", "#456789");
+d3.select("div#container1").style("background", "green");
+
+d3.select("p").style("color", "green"); //Select only the first paragraph
+
+// d3.selectAll: Se seleccionan todos las divisiones
+d3.selectAll("div").style("color", "white");
