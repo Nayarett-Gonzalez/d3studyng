12338134f1c2b3a9afe5cb0d6f177b941e8d8400
NayarettGonzalez <nagonzalezv@uc.cl>
2021-01-21 21:05:59 -0300

    Se optimiza la función

--- 022-brackets/app.js
+++ 022-brackets/app.js
@@ -3,7 +3,17 @@ const addClass = () => {
     d3.selectAll("circle:nth-child(odd)").classed("changeColor1", true);
 }
 
+// const changeBack = () => {
+//     d3.selectAll("circle:nth-child(even)").classed("changeColor", false);
+//     d3.selectAll("circle:nth-child(odd)").classed("changeColor1", false);
+// }
+
+// Vuelven a la posición inicial sólo
 const changeBack = () => {
-    d3.selectAll("circle:nth-child(even)").classed("changeColor", false);
-    d3.selectAll("circle:nth-child(odd)").classed("changeColor1", false);
+    d3.selectAll("circle").classed("changeColor", (d, i) => {
+        if (i % 2 == 0)
+            return true;
+        else
+            return false;
+    })
 }
