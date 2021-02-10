e82b1b8b0940ddc6e887c399cbab95c133f3124f
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-10 11:04:18 -0300

    Se generan los tamaños de los círculos en forma aleatoria

--- 024-attributes/app.js
+++ 024-attributes/app.js
@@ -1,3 +1,6 @@
 const update = () => {
-    d3.selectAll("circle").attr("r", 20);
+    // d3.selectAll("circle").attr("r", 20);
+     d3.selectAll("circle").attr("r", ()=>{
+         return Math.random()*40;
+     });
 }
