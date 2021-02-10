ef988a1d2c35665a0f23ca5a4d0916a974642fa7
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-10 19:02:09 -0300

    Se agrega botón y es transforma código a fn

--- 030-handsOn/app.js
+++ 030-handsOn/app.js
@@ -1,15 +1,19 @@
-var svg = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+const addSVG = () => {
 
-// svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
-// svg.append("circle").attr("r", 20).attr("cx", "100").attr("cy", "100").style("fill", "blue");
+    var svg = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
 
-
-// Otra forma
-
-svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
-svg.append("g").attr("transform", "translate(100,100)").append("circle").attr("r", 20);
+    // svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
+    // svg.append("circle").attr("r", 20).attr("cx", "100").attr("cy", "100").style("fill", "blue");
 
 
+    // Otra forma
 
-//Otra alternativa sin CSS
-// d3.select("body").append("svg").attr("width", 200).attr("height", 200).style("background","pink")
+    svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
+    svg.append("g").attr("transform", "translate(100,100)").append("circle").attr("r", 20).style("fill", "deeppink");
+
+
+
+    //Otra alternativa sin CSS
+    // d3.select("body").append("svg").attr("width", 200).attr("height", 200).style("background","pink")
+
+}
