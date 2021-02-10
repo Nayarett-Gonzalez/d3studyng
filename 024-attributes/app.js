7e229142129f97bb4edf8d8ab827ec1ca9d44f30
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-10 11:11:46 -0300

    Se varia la coordenada y

--- 024-attributes/app.js
+++ 024-attributes/app.js
@@ -1,6 +1,14 @@
 const update = () => {
     // d3.selectAll("circle").attr("r", 20);
-     d3.selectAll("circle").attr("r", ()=>{
-         return Math.random()*40;
-     });
+    // d3.selectAll("circle").attr("r", () => {
+    //     return Math.random() * 40;
+    // });
+
+    // d3.selectAll("circle").attr("cy", () => {
+    //     return Math.random() * 40;
+    // });
+
+    d3.selectAll("circle").attr("cy", (d, i) => {
+        return (i * 50) * Math.random();
+    });
 }
