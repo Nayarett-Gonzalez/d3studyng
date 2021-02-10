137f615b41a108d4151a603d350b7c350ff4eb66
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-10 18:28:41 -0300

    Se genera círculo en el centro de la figura

--- 030-handsOn/app.js
+++ 030-handsOn/app.js
@@ -1,1 +1,6 @@
-d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+var svg = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+
+svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
+svg.append("circle").attr("r", 20).attr("cx", "100").attr("cy", "100").style("fill", "blue");
+//Otra alternativa sin CSS
+// d3.select("body").append("svg").attr("width", 200).attr("height", 200).style("background","pink")
