ca34ba22346800a9dddc07c6db1e40345105f1f8
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-10 18:35:43 -0300

    Se genera círculo en el centro de la figura pero usando la propiedad transform

--- 030-handsOn/app.js
+++ 030-handsOn/app.js
@@ -1,6 +1,15 @@
 var svg = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
 
+// svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
+// svg.append("circle").attr("r", 20).attr("cx", "100").attr("cy", "100").style("fill", "blue");
+
+
+// Otra forma
+
 svg.append("rect").attr("width", 50).attr("height", 50).style("fill", "purple");
-svg.append("circle").attr("r", 20).attr("cx", "100").attr("cy", "100").style("fill", "blue");
+svg.append("g").attr("transform", "translate(100,100)").append("circle").attr("r", 20);
+
+
+
 //Otra alternativa sin CSS
 // d3.select("body").append("svg").attr("width", 200).attr("height", 200).style("background","pink")
